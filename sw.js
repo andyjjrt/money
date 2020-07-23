792793755b84305c1f00dd8bd2660fad39924c0e
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2020-07-23 09:05:29 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,6 +16,20 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
 self.addEventListener('offline', () => {
   console.log("offline")
 });
