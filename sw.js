968513a42c61722d1720629b8cb7901dec5d09d5
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2020-07-23 10:00:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,56 +1,52 @@
-const filesToCache = [
-  "https://andyjjrt.github.io/money",
-  "https://andyjjrt.github.io/money/script.js".
-  "https://raw.githubusercontent.com/andyjjrt/money/master/book_icon6765.ico".
-  "https://andyjjrt.github.io/money/manifest.json"
+'use strict';
+
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/offline.html',
 ];
 
-const cacheName = "static-online v1.5.1.16";
-const newName = "static-offline";
-
-// activate
-self.addEventListener("activate", event => {
-  console.log("[ServiceWorker]SW updated/installed,now waiting...");
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      var promiseArr = cacheNames.map(function(item) {
-        if (item !== cacheName) {
-          // Delete that cached file
-          return caches.delete(item);
-          console.log("[ServiceWorker] Removing old cache", item);
-        }
-      });
-      return Promise.all(promiseArr);
-    })
-  ); // end e.waitUntil
-});
-
-self.addEventListener("install", event => {
-  console.log("[ServiceWorker] SW updating/installing");
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-// fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return (
-        response ||
-        fetch(event.request).then(res =>
-          // 存 caches 之前，要先打開 caches.open(dataCacheName)
-          caches.open(cacheName).then(function(cache) {
-            // cache.put(key, value)
-            // 下一次 caches.match 會對應到 event.request
-            cache.put(event.request, res.clone());
-            return res;
-          })
-        )
-      );
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+  // CODELAB: Add fetch event handler here.
+if (evt.request.mode !== 'navigate') {
+  // Not a page navigation, bail.
+  return;
+}
+evt.respondWith(
+    fetch(evt.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+              .then((cache) => {
+                return cache.match('offline.html');
+              });
+        })
   );
 });
