61129ee72de49b0a0a91dd30a812ba8c026b1fb8
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2020-07-23 09:10:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,60 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  'https://andyjjrt.github.io/money/index.html',
-  'https://andyjjrt.github.io/money/script.js',
-  'book_icon6765.ico'
+const filesToCache = [
+  "https://andyjjrt.github.io/money",
+  "https://andyjjrt.github.io/money/script.js"
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+const cacheName = "static-online v1.5.1.16";
+const newName = "static-offline";
+
+var dlog = [];
+function logdate(msg) {
+  dlog.push(msg);
+  console.warn(dlog); // if you need to print the output
+}
+
+// activate
+self.addEventListener("activate", event => {
+  console.log("[ServiceWorker]SW updated/installed,now waiting...");
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
+    caches.keys().then(function(cacheNames) {
+      var promiseArr = cacheNames.map(function(item) {
+        if (item !== cacheName) {
+          // Delete that cached file
+          return caches.delete(item);
+          console.log("[ServiceWorker] Removing old cache", item);
         }
-        return fetch(event.request);
-      }
-    )
+      });
+      return Promise.all(promiseArr);
+    })
+  ); // end e.waitUntil
+});
+
+self.addEventListener("install", event => {
+  console.log("[ServiceWorker] SW updating/installing");
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
-self.addEventListener('offline', () => {
-  console.log("offline")
+// fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return (
+        response ||
+        fetch(event.request).then(res =>
+          // 存 caches 之前，要先打開 caches.open(dataCacheName)
+          caches.open(cacheName).then(function(cache) {
+            // cache.put(key, value)
+            // 下一次 caches.match 會對應到 event.request
+            cache.put(event.request, res.clone());
+            return res;
+          })
+        )
+      );
+    })
+  );
 });
