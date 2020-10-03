4b9a05f8e50b19ea6e7227f700990c3ee7a4c88d
andyjjrt <60731530+andyjjrt@users.noreply.github.com>
2020-10-03 21:53:07 +0800

    v3.0-alpha.1.01
    -修正標籤無窮迴圈導致的當機問題
    -修正消失的前往按鈕

--- script.js
+++ script.js
@@ -190,8 +190,8 @@ function init2(a){
       if(a == arr2[i]){
         init3(a)
         $("#dropdownMenu2").html('<svg width="1em" height="1em" viewbox="0 0 16 16" class="bi bi-funnel" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/></svg>&nbsp;' + a)
-        for(var i = 0;i<datas.length;i++){
-          var j = datas.length-1-i
+        for(var k = 0;k<datas.length;k++){
+          var j = datas.length-1-k
           var ti = datas[j][0].split("T");
           var ti2 = ti[0].split("-")
           var ti3 = ti2[1] + ti2[2]
